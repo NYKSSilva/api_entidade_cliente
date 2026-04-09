7c546a8439d8322cd3830079181ee6d14c3f5d1f
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-04-09 09:31:10 -0300

    Api de entidade

---
+++ entidade_cliente/src/main/java/br/com/senai/entidade_cliente/EntidadeClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.entidade_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadeClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadeClienteApplication.class, args);
+	}
+
+}
